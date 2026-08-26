da8e17fa2fa2f02b6289cc8c39aac7cdb07b33a9
ProNewb <26541297@students.lincoln.ac.uk>
2026-08-26 15:27:18 +0100

    Create result_destination_dialog.py

---
+++ ui/dialogs/result_destination_dialog.py
@@ -0,0 +1,77 @@
+from PySide6.QtWidgets import (
+    QButtonGroup,
+    QCheckBox,
+    QDialog,
+    QGridLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QScrollArea,
+    QSplitter,
+    QVBoxLayout,
+    QLabel,
+    QComboBox,
+    QLineEdit,
+    QPushButton,
+    QSizePolicy,
+    QWidget
+)
+class ResultDestinationDialog(QDialog):
+
+    REPLACE_RESULT = "replace_result"
+    CREATE_RESULT = "create_result"
+    REPLACE_MAIN = "replace_main"
+
+    def __init__(
+        self,
+        has_result=False,
+        parent=None
+    ):
+        super().__init__(parent)
+
+        self.setWindowTitle("Result Destination")
+
+        layout = QVBoxLayout(self)
+
+        layout.addWidget(
+            QLabel("Where should the result go?")
+        )
+
+        self.destination_combo = QComboBox()
+
+        if has_result:
+            self.destination_combo.addItem(
+                "Replace current result",
+                self.REPLACE_RESULT
+            )
+
+        self.destination_combo.addItem(
+            "Create new result",
+            self.CREATE_RESULT
+        )
+
+        self.destination_combo.addItem(
+            "Replace Main with result",
+            self.REPLACE_MAIN
+        )
+
+        layout.addWidget(
+            self.destination_combo
+        )
+
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok |
+            QDialogButtonBox.Cancel
+        )
+
+        buttons.accepted.connect(
+            self.accept
+        )
+
+        buttons.rejected.connect(
+            self.reject
+        )
+
+        layout.addWidget(buttons)
+
+    def destination(self):
+        return self.destination_combo.currentData()
